4de491366bd946f5af2395d20d162fad3e22afd4
Patrick <patrickbellot77@gmail.com>
2016-11-25 17:01:18 -0600

    Getting list working

--- GList/ViewController.swift
+++ GList/ViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
+  var items = [String]()
+  
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var tableView: UITableView!
+ 
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+    }
+  
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return items.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell") as! TextInputTableViewCell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
     
-    cell.configure(text: "", placeholder: "Enter an item")
-    return cell
+    cell?.textLabel?.text = items[indexPath.row]
+    cell?.textLabel?.textColor = UIColor.green
+    return cell!
   }
   
+    @IBAction func addButton(_ sender: Any) {
+      
+      let newItem = textField.text
+      items.append(newItem!)
+      textField.resignFirstResponder()
+      textField.text = ""
+      textField.placeholder = "Add Item"
+      tableView.reloadData()
+    }
 }
